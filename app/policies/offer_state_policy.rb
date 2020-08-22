f9dd0dea832b525ea03b77bfc001d99ed359b679
Lyon Oliveira <lyoncesar@gmail.com>
2020-08-22 18:54:21 -0300

    Atualiza políticas para atualização de estado da offerta.

--- app/policies/offer_state_policy.rb
+++ app/policies/offer_state_policy.rb
@@ -1,30 +1,65 @@
 class OfferStatePolicy
   def initialize(starts_at, ends_at)
-    @starts_at = starts_at
-    @ends_at = ends_at
+    @starts_at = starts_at.strftime("%Y %m %d") unless starts_at.blank?
+    @ends_at = ends_at.strftime("%Y %m %d") unless ends_at.blank?
+    @admin_user = admin_user
   end
 
   def can_enable?
-    return true if current_day >= starts_at
+    return true if today_greater_or_equal_start && ends_blank?
+    return true if today_greater_or_equal_start && today_less_or_equal_end
+    return false if today_greater_or_equal_start && today_greater_or_equal_end
 
     false
   end
 
   def can_disable?
-    if ends_at.blank?
-      false
-    elsif current_day > ends_at
-      true
-    else
-      false
-    end
+    return true if starts_blank? || today_less_start
+    return false if ends_blank? && today_greater_or_equal_start
+    return false if ends_blank?
+    return true if today_greater_end
+    return false if today_equal_end && today_greater_or_equal_start
+
+    false
   end
 
   private
 
-  attr_reader :starts_at, :ends_at
+  attr_reader :starts_at, :ends_at, :admin_user
 
-  def current_day
-    Date.today
+  def today_greater_or_equal_start
+    today >= starts_at
+  end
+
+  def today_greater_or_equal_end
+    today >= ends_at
+  end
+
+  def today_less_start
+    today < starts_at
+  end
+
+  def today_less_or_equal_end
+    today <= ends_at
+  end
+
+  def today_equal_end
+    today == ends_at
+  end
+
+  def starts_blank?
+    starts_at.blank?
+  end
+
+  def ends_blank?
+    ends_at.blank?
+  end
+
+  def today_greater_end
+    today > ends_at
+  end
+
+  def today
+    Date.today.strftime("%Y %m %d")
   end
 end
--- spec/policies/offer_state_policy_spec.rb
+++ spec/policies/offer_state_policy_spec.rb
@@ -5,49 +5,158 @@ RSpec.describe OfferStatePolicy do
 
   context '.can_enable?' do
     context 'quando hoje é o início da oferta' do
-      it 'retorna verdadeiro' do
-        offer.starts_at = Date.today
-        policy = described_class.new(offer.starts_at, offer.ends_at)
+      context 'e o encerramento é futuro' do
+        it 'retorna verdadeiro' do
+          offer.starts_at = 0.day.from_now
+          offer.ends_at = 3.days.from_now
+          policy = described_class.new(offer.starts_at, offer.ends_at)
 
-        expect(policy.can_enable?).to be_truthy
+          expect(policy.can_enable?).to be_truthy
+        end
+      end
+
+      context 'e o encerramento é hoje' do
+        it 'retorna verdadeiro' do
+          offer.starts_at = 0.day.from_now
+          offer.ends_at = 0.day.from_now
+          policy = described_class.new(offer.starts_at, offer.ends_at)
+
+          expect(policy.can_enable?).to be_truthy
+        end
+      end
+
+      context 'e o encerramento era ontem' do
+        it 'retorna false' do
+          offer.starts_at = 0.day.from_now
+          offer.ends_at = 1.day.ago
+          policy = described_class.new(offer.starts_at, offer.ends_at)
+
+          expect(policy.can_enable?).to be_falsey
+        end
+      end
+
+      context 'e o encerramento está vazio' do
+        it 'retorna verdadeiro' do
+          offer.starts_at = 0.day.from_now
+          offer.ends_at = nil
+          policy = described_class.new(offer.starts_at, offer.ends_at)
+
+          expect(policy.can_enable?).to be_truthy
+        end
       end
     end
 
     context 'quando a oferta ainda não iniciou' do
-      it 'retorna falso' do
-        offer.starts_at = 3.days.from_now
-        policy = described_class.new(offer.starts_at, offer.ends_at)
+      context 'e o encerramento da oferta é futuro' do
+        it 'retorna falso' do
+          offer.starts_at = 3.days.from_now
+          offer.ends_at = 5.days.from_now
+          policy = described_class.new(offer.starts_at, offer.ends_at)
 
-        expect(policy.can_enable?).to be_falsey
+          expect(policy.can_enable?).to be_falsey
+        end
+      end
+
+      context 'e o encerramento da oferta já passou' do
+        it 'retorna falso' do
+          offer.starts_at = 3.days.from_now
+          offer.ends_at = 1.day.ago
+          policy = described_class.new(offer.starts_at, offer.ends_at)
+
+          expect(policy.can_enable?).to be_falsey
+        end
+      end
+
+      context 'e o encerramento da oferta está vazio' do
+        it 'retorna falso' do
+          offer.starts_at = 3.days.from_now
+          offer.ends_at = nil
+          policy = described_class.new(offer.starts_at, offer.ends_at)
+
+          expect(policy.can_enable?).to be_falsey
+        end
+      end
+    end
+
+    context 'quando já passou a data de encerramento da oferta' do
+      context 'e a data de inicio é o dia anterior' do
+        it 'retorna falso' do
+          offer.starts_at = 1.day.ago
+          offer.ends_at = 1.day.ago
+          policy = described_class.new(offer.starts_at, offer.ends_at)
+
+          expect(policy.can_enable?).to be_falsey
+        end
+      end
+
+      context 'e a data de inicio é hoje' do
+        it 'retorna falso' do
+          offer.starts_at = 0.day.from_now
+          offer.ends_at = 1.day.ago
+          policy = described_class.new(offer.starts_at, offer.ends_at)
+
+          expect(policy.can_enable?).to be_falsey
+        end
+      end
+
+      context 'e a data de inicio é futura' do
+        it 'retorna falso' do
+          offer.starts_at = 1.day.from_now
+          offer.ends_at = 1.day.ago
+          policy = described_class.new(offer.starts_at, offer.ends_at)
+
+          expect(policy.can_enable?).to be_falsey
+        end
       end
     end
   end
 
   context '.can_disable?' do
-    context 'quando o encerramento da oferta é hoje' do
-      it 'retorna falso' do
-        offer.ends_at = Date.today
-        policy = described_class.new(offer.starts_at, offer.ends_at)
+    let(:offer) { create(:offer_enabled) }
 
-        expect(policy.can_disable?).to be_falsey
+    context 'quando o encerramento da oferta é hoje' do
+      context 'e a data de início foi o dia anterior' do
+        it 'retorna falso' do
+          offer.starts_at = 1.day.ago
+          offer.ends_at = 0.day.from_now
+          policy = described_class.new(offer.starts_at, offer.ends_at)
+
+          expect(policy.can_disable?).to be_falsey
+        end
+      end
+
+      context 'e a data de início é hoje' do
+        it 'retorna falso' do
+          offer.starts_at = 0.day.from_now
+          offer.ends_at = 0.day.from_now
+          policy = described_class.new(offer.starts_at, offer.ends_at)
+
+          expect(policy.can_disable?).to be_falsey
+        end
       end
     end
 
     context 'quando o encerramento da oferta já passou' do
-      it 'retorna verdadeiro' do
-        offer.ends_at = 3.days.ago
-        policy = described_class.new(offer.starts_at, offer.ends_at)
+      context 'e a data de início já passou' do
+        it 'retorna verdadeiro' do
+          offer.starts_at = 4.days.ago
+          offer.ends_at = 3.days.ago
+          policy = described_class.new(offer.starts_at, offer.ends_at)
 
-        expect(policy.can_disable?).to be_truthy
+          expect(policy.can_disable?).to be_truthy
+        end
       end
     end
 
     context 'quando o encerramento da oferta não é preenchido' do
-      it 'retorna falso' do
-        offer.ends_at = nil
-        policy = described_class.new(offer.starts_at, offer.ends_at)
+      context 'e o início da oferta é hoje' do
+        it 'retorna falso' do
+          offer.starts_at = 0.day.from_now
+          offer.ends_at = nil
+          policy = described_class.new(offer.starts_at, offer.ends_at)
 
-        expect(policy.can_disable?).to be_falsey
+          expect(policy.can_disable?).to be_falsey
+        end
       end
     end
   end
