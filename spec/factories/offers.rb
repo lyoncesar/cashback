93840bf309a13f9b795957f560a0aa6fccd4166a
Lyon Oliveira <lyoncesar@gmail.com>
2020-08-22 20:50:18 -0300

    Adiciona factiories para ofertas que podem ou não serem desativadas.

--- spec/factories/offers.rb
+++ spec/factories/offers.rb
@@ -17,4 +17,24 @@ FactoryBot.define do
     description { Faker::Lorem.characters(number: 50) }
     state { :enabled }
   end
+
+  factory :offer_may_disable, class: Offer do
+    advertiser_name { Faker::Company.name }
+    url { Faker::Internet.url }
+    premium { false }
+    starts_at { 1.day.ago }
+    ends_at { 1.day.ago }
+    description { Faker::Lorem.characters(number: 50) }
+    state { :enabled }
+  end
+
+  factory :offer_dont_disable, class: Offer do
+    advertiser_name { Faker::Company.name }
+    url { Faker::Internet.url }
+    premium { false }
+    starts_at { 1.day.ago }
+    ends_at { nil }
+    description { Faker::Lorem.characters(number: 50) }
+    state { :enabled }
+  end
 end
