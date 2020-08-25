71fa1771c4464717b6ec02052290406c36b3daac
Lyon Oliveira <lyoncesar@gmail.com>
2020-08-24 23:18:40 -0300

    Isola helpers do header
    Antes eles estavam no helper da home.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,33 +1,4 @@
 module HomeHelper
-  def user_signed_in_email
-    if user_signed_in?
-      content_tag(:span, "#{current_user.email}", :class => 'navbar-text text-light nav-righ')
-    end
-  end
-  def user_signed_in_edit
-    if user_signed_in?
-      content_tag(:li, class: "nav-item") do
-        link_to 'Editar perfil', edit_user_registration_path, :class => 'nav-link text-light nav-righ'
-      end
-    else
-      content_tag(:li, class: "nav-item") do
-        link_to "Register", new_user_registration_path, :class => 'nav-link text-light nav-righ'
-      end
-    end
-  end
-
-  def user_signed_in_logout
-    if user_signed_in?
-      content_tag(:li, class: "nav-item") do
-        link_to "Logout", destroy_user_session_path, method: :delete, :class => 'nav-link text-light nav-righ'
-      end
-    else
-      content_tag(:li, class: "nav-item") do
-        link_to "Login", new_user_session_path, :class => 'nav-link text-light nav-righ'
-      end
-    end
-  end
-
   def admin_offers
     if current_user.admin?
       content_tag(:div) do
