e5ad075f3b36263b9406af070a9a1a434ab2cf51
Lyon Oliveira <lyoncesar@gmail.com>
2020-08-26 04:28:18 -0300

    Atualiza testes do helper de ofertas
    O retorno do método foi alterado para retornar um valor com titleize, mas o valor esperado nos
    testes era com downcase.

--- spec/helpers/offers_helper_spec.rb
+++ spec/helpers/offers_helper_spec.rb
@@ -14,13 +14,13 @@ RSpec.describe OffersHelper, type: :helper do
   describe 'offer_new_state_option' do
     context 'quando o estado informado é enabled' do
       it 'retorna disable' do
-        expect(helper.offer_new_state_option('enabled')).to eq('disable')
+        expect(helper.offer_new_state_option('enabled')).to eq('Disable')
       end
     end
 
     context 'quando o estado informado é disabled' do
       it 'retorna o enable' do
-        expect(helper.offer_new_state_option('disabled')).to eq('enable')
+        expect(helper.offer_new_state_option('disabled')).to eq('Enable')
       end
     end
   end
@@ -28,13 +28,13 @@ RSpec.describe OffersHelper, type: :helper do
   describe 'offer_new_state' do
     context 'quando o estado atual é enabled' do
       it 'retorna disabled' do
-        expect(helper.offer_new_state('enabled')).to eq('disabled')
+        expect(helper.offer_new_state('enabled')).to eq('Disabled')
       end
     end
 
     context 'quando o estado atual é disabled' do
       it 'retorna enabled' do
-        expect(helper.offer_new_state('disabled')).to eq('enabled')
+        expect(helper.offer_new_state('disabled')).to eq('Enabled')
       end
     end
   end
