23f26d09b5ca5bc4324db08a058cd91c8307fa5e
Lyon Oliveira <lyoncesar@gmail.com>
2020-08-27 05:31:04 -0300

    Adiciona estilo personalisado para definir a altura da classe container do bootstrap.
    Para isso foi necessário adicionar o arquivo css no application.js, por causa do webpacker.
    Já aproveitei e adicionei outras estilos que estavam inline e separei por arquivos.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,10 @@
 import Rails from "@rails/ujs"
 
 import 'jquery'
-import('src/plugins')
-
+import 'src/plugins'
+import 'stylesheets/application'
+import 'stylesheets/home'
+import 'stylesheets/offers'
 import 'css/site'
 
 Rails.start()
