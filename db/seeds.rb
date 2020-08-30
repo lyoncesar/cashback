b2ceccbb5b30db0afbc0c8d51030e39ecebe9204
Lyon Oliveira <lyoncesar@gmail.com>
2020-08-30 16:54:54 -0300

    Adiciona ofertas ao seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+10.times do |n|
+  Offer.create(
+    advertiser_name: Faker::Company.name,
+    url: Faker::Internet.url,
+    premium: false,
+    starts_at: 1.day.ago,
+    description: Faker::Lorem.characters(number: 50)
+  )
+end
+
+5.times do |n|
+  Offer.create(
+    advertiser_name: Faker::Company.name,
+    url: Faker::Internet.url,
+    premium: false,
+    starts_at: Date.today,
+    description: Faker::Lorem.characters(number: 50),
+    state: :enabled
+  )
+end
+
+5.times do |n|
+  Offer.create(
+    advertiser_name: Faker::Company.name,
+    url: Faker::Internet.url,
+    premium: true,
+    starts_at: 1.day.ago,
+    ends_at: 0.day.from_now,
+    description: Faker::Lorem.characters(number: 50),
+    state: :enabled
+  )
+end
