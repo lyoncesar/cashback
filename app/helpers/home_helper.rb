c563ea2e98b2f24e7ce71ab31bdbc4a91af06131
Lyon Oliveira <lyoncesar@gmail.com>
2020-08-30 20:40:44 -0300

    Adiciona I18n a tela ofertas do dia.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,7 +2,7 @@ module HomeHelper
   def admin_offers
     if current_user.admin?
       content_tag(:div) do
-        link_to "Offers admin", offers_path, :class => 'btn btn-link float-right'
+        link_to t('home.offers_admin'), offers_path, :class => 'btn btn-link float-right'
       end
     end
   end
@@ -10,7 +10,7 @@ module HomeHelper
   def offer_premium(offer_premium)
     if offer_premium
       content_tag(:div, class: 'text-right') do
-        content_tag(:span, 'Premium', class: 'badge badge-danger badge-premium border border-light rounded-pill position-absolute  shadow')
+        content_tag(:span, t('home.premium'), class: 'badge badge-danger badge-premium border border-light rounded-pill position-absolute  shadow')
       end
     end
   end
