b0965e25b9c274400208ab7aeda60471a024040a
Lyon Oliveira <lyoncesar@gmail.com>
2020-08-30 23:10:18 -0300

    Adiciona I18n na tela de edição de usuário.

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,9 +1,9 @@
 module DeviseHelper
   def minimum_password_length(min_pass_length)
     if min_pass_length
-      "Password (#{min_pass_length} minimum)"
+      t('users.password_min_length', min_length: min_pass_length)
     else
-      'Password'
+      t('users.password')
     end
   end
 end
